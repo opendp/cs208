d33e67606b47f3a03bc9712ed8f15ffce5292335
Shoeboxam <shoeboxam@gmail.com>
2022-02-05 11:36:37 -0500

    hw2_starter.py consistency

--- spring2022/homework/hw2_starter.py
+++ spring2022/homework/hw2_starter.py
@@ -8,12 +8,14 @@ import pandas as pd
 # a dataframe containing the data to be attacked
 data: pd.DataFrame = pd.read_csv(
     "https://raw.githubusercontent.com/opendp/cs208/main/spring2022/data/FultonPUMS5reconstruction.csv")
-TARGET = "uscitizen"
 
 # names of public identifier columns
 pub = ["sex", "age", "educ", "latino", "black", "asian",
        "married", "divorced", "children", "disability",
        "militaryservice", "employed", "englishability"]
+
+# variable to reconstruct
+target = "uscitizen"
 
 
 def execute_subsetsums_exact(predicates):
@@ -23,7 +25,7 @@ def execute_subsetsums_exact(predicates):
 
     :param predicates: a list of predicates on the public variables
     :returns a 1-d np.ndarray of exact answers the subset sum queries"""
-    return data[TARGET].values @ np.stack([pred(data) for pred in predicates], axis=1)
+    return data[target].values @ np.stack([pred(data) for pred in predicates], axis=1)
 
 
 if __name__ == "__main__":
@@ -54,16 +56,15 @@ if __name__ == "__main__":
 
 # Problem 1.a
 # TODO: Write the reconstruction function!
-#       Your signature might look like this:
 def reconstruction_attack(data_pub, predicates, answers):
     """Reconstructs a target column based on the `answers` to queries about `data`.
 
     :param data_pub: data of length n consisting of public identifiers
     :param predicates: a list of k predicate functions
     :param answers: a list of k answers to a query on data filtered by the k predicates
-    :return: 1-dimensional boolean ndarray"""
+    :return 1-dimensional boolean ndarray"""
     pass
 
 
-# Problem 2
-# See membership_attack.ipynb, and accompanying practicum 2/3 lecture.
+# Problem 1.d
+# see /spring2022/examples/wk1_attacks/membership_attack.ipynb
