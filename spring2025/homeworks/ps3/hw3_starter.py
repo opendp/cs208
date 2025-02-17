9b97b270e1c2f244cbda70f18f800a7459e8ca61
Zach <zacharyratliff@g.harvard.edu>
2025-02-16 21:43:33 -0500

    update ps3

--- spring2025/homeworks/ps3/hw3_starter.py
+++ spring2025/homeworks/ps3/hw3_starter.py
@@ -11,34 +11,37 @@ pub = ["id", "age", "sex", "blood"]
 # variable to reconstruct
 target = "invoice"
 
+epsilon = 0.1
 
 # Returns the ULP in base-2 of a floating-point number
 def get_ulp_base2(x):
     return math.log2(math.ulp(x))
 
-# Release a DP sum over the invoice column for patients in the dataset with ID = id
-# Fill in the sensitivity and scale parameters
-def release_dp_sum(id, epsilon):
-    sensitivity = 
-    scale = 
-    sensitive_sum = data.loc[data['id'] == id, 'invoice'].sum()
-    return sensitive_sum + np.random.laplace(loc=0.0, scale=scale)
-
+# Release a DP count over the invoice column for patients in the dataset with ID = id
+def release_dp_count(query):
+    sensitivity = 1
+    scale = sensitivity/epsilon
+    sensitive_count = query(data)
+    return sensitive_count + np.random.laplace(loc=0.0, scale=scale)
 
 # Implement the hypothesis test!
 def hypothesis_test(observed):
     pass
 
-epsilon = 0.1
 num_experiments = 10000
 
 # Run the experiments
 for _ in range(num_experiments):
+
     # Randomly choose a patient ID
     id = np.random.randint(0, len(data))
 
-    # Receive a DP sum over the patient's invoice value
-    observed = release_dp_sum(id, epsilon)
+    # Example counting query...
+    # counts the number of individuals in the dataset patient ID = id and invoice amount = 1000
+    # you'll want to update this!
+    observed = release_dp_count(
+        lambda data: (data.loc[data['id'] == id, 'invoice'] == 1000).sum()
+    )
 
     # 1 = reject null, 0 otherwise
     decision = hypothesis_test(observed)
